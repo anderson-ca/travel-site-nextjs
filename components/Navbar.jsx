da5f48415b5e00f2de6303809c9af0f32868e54d
anderson <anderson.d.cardoso92@gmail.com>
2022-01-20 13:36:40 -0600

    add menu icon event handler

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,23 +1,34 @@
 import navbarStyles from '../styles/Navbar.module.css';
 import Link from 'next/link';
 
-import { FaTypo3 } from 'react-icons/fa';
-
+import { useState } from 'react';
+import { FaTypo3, FaTimes, FaBars } from 'react-icons/fa';
+// click == <FaTimes /> ? setClick(<FaBars />) : setClick(<FaTimes />);
 const Navbar = () => {
-  return (
-      <>
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <Link href='/' passHref>
-                    <span>
-                        TRVL
-                        <FaTypo3 />
-                    </span>
-                </Link>
-            </div>
-        </nav>
-      </>
-  );
+    const [click, setClick] = useState(<FaTimes />);
+
+    const handleClick = () => click.type.name == 'FaTimes' ? setClick(<FaBars />) : setClick(<FaTimes />);
+
+
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link href='/' className='navbar-logo' passHref>
+                        <span>
+                            TRVL
+                            <FaTypo3 />
+                        </span>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <p>
+                            {click}
+                        </p>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default Navbar;
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>home component</h1>
+      
     </div>
   )
 }
